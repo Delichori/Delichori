61200c956bef476d056c995b8263ac738563380a
Delichori <delichorilagloria@gmail.com>
2023-10-25 18:56:47 -0500

    subida_inicial_yessi
    subida_inicial_yessi25/10

---
+++ src/main/java/co/com/delichori/model/Producto.java
@@ -0,0 +1,21 @@
+package co.com.delichori.model;
+
+public class Producto {
+
+    private int idProducto;
+    private String nombreProducto;
+    private String descripcionProducto;
+    private double precioCosto;
+    private double precioVenta;
+    private double ganancia_producto;
+    private double existencia_producto;
+    private int opc;
+
+//PREGUNTARLE A JOHA DONDE SE HACE LA FÓRMULA DE LA GANANCIA
+
+    public Producto() {
+
+    }
+}
+
+
