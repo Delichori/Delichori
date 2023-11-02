d1e8ec9166ce56d9219d7ecbeff9c7c8aa307bcc
Joloa <delichorilagloria@gmail.com>
2023-11-01 22:39:44 -0500

    Actualización Johana
    01/11

--- src/main/java/co/com/delichori/dao/AdministradorDao.java
+++ src/main/java/co/com/delichori/dao/AdministradorDao.java
@@ -1,13 +1,76 @@
 package co.com.delichori.dao;
+import co.com.delichori.conexion.Conexion;
+import co.com.delichori.model.Cliente;
+import co.com.delichori.model.Usuario;
+import co.com.delichori.service.UsuarioService;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class AdministradorDao {
-    public static void registrarUsuarioDB(){
+    public static void registrarAdministradorDB(Usuario registro){
 
+        try(Connection conexion = Conexion.get_connection()){//Inicia la conexión
+
+            PreparedStatement ps = null; //Sirve para prepara el sql de la DB, preparar sentencia de SQL
+
+            try {
+
+                String query = "INSERT INTO cliente(cedula, nombre, apellido, celular, email, clave)VALUES(?,?,?,?,?,?)";
+
+                ps = conexion.prepareStatement(query);
+
+                ps.setInt(1, registro.getCedula());
+                ps.setString(2, registro.getNombre());
+                ps.setString(3, registro.getApellido());
+                ps.setLong(4, registro.getCelular());
+                ps.setString(6, registro.getEmail());
+                ps.setString(7, registro.getClave());
+
+                ps.executeUpdate();
+
+                System.out.println("Registro de administrador exitoso");
+
+            }catch (SQLException e){
+                System.out.println(e);
+            }
+        }catch (SQLException e){
+
+            System.out.println(e);
+
+        }finally {
+            Conexion.close_connection();
+        }
     }
+    public static void eliminarAdministradorDB(int idAdministrador){
+
+        try(Connection connect = Conexion.get_connection()){
+
+            PreparedStatement ps = null;
+
+            try {
+                String query = "DELETE FROM producto where producto.id = ?";
+                ps = connect.prepareStatement(query);
+                ps.setInt(1, idAdministrador);
+                ps.executeUpdate();
+                System.out.println("El registro ha sido eliminado correctamente");
+            }catch (SQLException e){
+                System.out.println("No se eliminó el registro");
+                System.out.println(e);
+            }
+
+        }catch (SQLException e){
+            System.out.println(e);
+        }finally {
+            Conexion.close_connection();
+        }
+    }
+
     public static void iniciarSesionDB(){
 
-    }
-    public static void cerrarSesionDB(){
+
 
     }
 }
+
--- src/main/java/co/com/delichori/dao/ClienteDao.java
+++ src/main/java/co/com/delichori/dao/ClienteDao.java
@@ -1,14 +1,76 @@
 package co.com.delichori.dao;
+import co.com.delichori.conexion.Conexion;
+import co.com.delichori.model.Cliente;
+import co.com.delichori.model.Usuario;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class ClienteDao {
 
-    public static void registrarUsuarioDB(){
+    public static void registrarUsuarioDB(Usuario registro){
 
-    }
-    public static void iniciarSesionDB(){
+        try(Connection conexion = Conexion.get_connection()){//Inicia la conexión
 
+            PreparedStatement ps = null; //Sirve para prepara el sql de la DB, preparar sentencia de SQL
+
+            try {
+
+                String query = "INSERT INTO cliente(cedula, nombre, apellido, celular, direccion, email, clave)VALUES(?,?,?,?,?,?,?)";
+
+                ps = conexion.prepareStatement(query);
+
+                Cliente registroCliente = new Cliente();
+
+                ps.setInt(1, registro.getCedula());
+                ps.setString(2, registro.getNombre());
+                ps.setString(3, registro.getApellido());
+                ps.setLong(4, registro.getCelular());
+                ps.setString(5, registroCliente.getDireccion());
+                ps.setString(6, registro.getEmail());
+                ps.setString(7, registro.getClave());
+
+                ps.executeUpdate(); //Insert, delete y update
+
+                System.out.println("Registro de usuario exitoso");
+
+            }catch (SQLException e){
+                System.out.println(e);
+            }
+        }catch (SQLException e){
+
+            System.out.println(e);
+
+        }finally { //Sin importar si el try falla o no el finally se ejecuta
+            Conexion.close_connection();
+        }
     }
-    public static void cerrarSesionDB(){
+    public static void eliminarUsuarioDB(int idCliente){
+
+        try(Connection connect = Conexion.get_connection()){
+
+            PreparedStatement ps = null;
+
+            try {
+                String query = "DELETE FROM cliente where cliente.id = ?";
+                ps = connect.prepareStatement(query);
+                ps.setInt(1, idCliente);
+                ps.executeUpdate();
+                System.out.println("El registro ha sido eliminado correctamente");
+            }catch (SQLException e){
+                System.out.println("No se eliminó el registro");
+                System.out.println(e);
+            }
+
+        }catch (SQLException e){
+            System.out.println(e);
+        }finally {
+            Conexion.close_connection();
+        }
+    }
+    public static void iniciarSesionDB(Usuario email){
 
     }
 }
+
