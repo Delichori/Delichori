f36381bbaebf8ab11b456f8e237c558109c70670
Delichori <delichorilagloria@gmail.com>
2023-11-02 15:48:24 -0500

    actualizacion3 novi 02 yessi
    actualizacion3 novi 02 yessi

--- src/main/java/co/com/delichori/dao/PedidoDao.java
+++ src/main/java/co/com/delichori/dao/PedidoDao.java
@@ -3,10 +3,7 @@ import co.com.delichori.conexion.Conexion;
 import co.com.delichori.model.Pedido;
 import co.com.delichori.service.PedidoService;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class PedidoDao {
 
@@ -18,18 +15,18 @@ public class PedidoDao {
             PreparedStatement ps = null;
 
             try {
-                String query = "INSERT INTO pedido(EstadoPedido, CantidadChorizo, CantidadChorizoPicante, ValorTotalPedido, FechaPedido, FechaEntrega) VALUES (?,?,?,?,?,?)";
+                String query = "INSERT INTO pedido(EstadoPedido, CantidadProducto, ValorTotalPedido, FechaPedido, FechaEntrega) VALUES (?,?,?,?,?)";
 
                 ps = conexion.prepareStatement(query);
 
                 ps.setString(1,registro.getEstadoPedido());
                 ps.setInt(2, registro.getCantidadProducto());
+                ps.setDouble(3, registro.getValorTotalPedido());
+                ps.setDate(4, Date.valueOf(registro.getFechaPedido()));
+                ps.setDate(5, Date.valueOf(registro.getFechaEntrega()));
 
-                /*ps.setString(2,registro.getDescripcionProducto());
-                ps.setDouble(3,registro.getPrecio());
-                ps.setDouble(4,registro.getCosto());
-                ps.setDouble(5, registro.getCantidad());*/
 
+                //
                 ps.executeUpdate();
 
                 System.out.println("Registro de pedido exitoso ");
